8e9d2e503024961e46943920d519d855263b1863
DavidChupryna <davchup45@gmail.com>
2024-05-21 22:28:14 +0300

    Добавил модуль GPT.py, также немного доработал модуль config.py

--- config.py
+++ config.py
@@ -1,6 +1,20 @@
+import configparser
+
+config = configparser.ConfigParser()
+
 BOT_TOKEN = "7032292704:AAFChMlXgB41IlKs8CSOJxYBcB2C2buacpM"
 
 HOME_PATH = '~'
 LOGS_PATH = f'{HOME_PATH}/logs.txt' #у меня здесь вылезает ошибка (Нияз)
 DB_FILE = 'database.db'
 DB_TABLE_NAME = 'users'
+
+config['GPT'] = {
+    'URL': 'https://llm.api.cloud.yandex.net/foundationModels/v1/completion',
+    'TEMPERATURE': '0.6',
+    'TOKENIZE_URL': 'https://llm.api.cloud.yandex.net/foundationModels/v1/tokenizeCompletion',
+}
+
+SYSTEM_PROMPT = [{'role': 'system', 'text': 'Ты веселый помощник по путешествиям. '
+                                            'Подробно рассказывай о достопримечательностях выбранного пользователем город '
+                                            'Поддерживай диалог.'}]
---
+++ gpt.py
@@ -0,0 +1,59 @@
+import logging
+import requests
+from config import config, SYSTEM_PROMPT
+from creds import get_creds
+
+logging.basicConfig(filename='logs.txt', level=logging.INFO,
+                    format="%(asctime)s FILE: %(filename)s IN: %(funcName)s MESSAGE: %(message)s", filemode="w")
+
+iam_token, folder_id = get_creds()
+
+
+def count_gpt_tokens(messages):
+    url = config['GPT']['TOKENIZE_URL']
+    headers = {
+        'Authorization': f'Bearer {iam_token}',
+        'Content-Type': 'application/json'
+    }
+    data = {
+        'modelUri': f"gpt://{folder_id}/yandexgpt-lite",
+        "messages": messages
+    }
+    try:
+        return len(requests.post(url=url, json=data, headers=headers).json()['tokens'])
+    except Exception as e:
+        logging.error(e)
+        return 0
+
+
+def ask_gpt(messages):
+    url = config['GPT']['URL']
+    headers = {
+        'Authorization': f'Bearer {iam_token}',
+        'Content-Type': 'application/json'
+    }
+
+    data = {
+        'modelUri': f"gpt://{folder_id}/yandexgpt-lite",
+        "completionOptions": {
+            "stream": False,
+            "temperature": 0.7,
+            "maxTokens": int(config['LIMITS']['MAX_ANSWER_GPT_TOKENS'])
+        },
+        "messages": SYSTEM_PROMPT + messages
+    }
+
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        print(response.json())
+        logging.info('GPT: request sent!')
+        if response.status_code != 200:
+            return False, f"Ошибка GPT. Статус-код: {response.status_code}", None
+
+        answer = response.json()['result']['alternatives'][0]['message']['text']
+        tokens_in_answer = count_gpt_tokens([{'role': 'assistant', 'text': answer}])
+        return True, answer, tokens_in_answer
+
+    except Exception as e:
+        logging.error(e)
+        return False, "Ошибка при обращении к GPT", None
