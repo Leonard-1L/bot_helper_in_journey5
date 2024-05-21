c361604ce246fb6d64d36983607b2eb8df05eb15
ÐLeonard-1L <lprofi52@gmail.com>
2024-05-21 21:18:40 +0300

    cheking commit

--- bot.py
+++ bot.py
@@ -16,15 +16,18 @@ bot = telebot.TeleBot(token=BOT_TOKEN)
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     markup = InlineKeyboardMarkup()
-    itembtn = InlineKeyboardButton(text="Нажми меня!", callback_data='data')
-    markup.add(itembtn)
+    itembtn1 = InlineKeyboardButton(text="Нажми меня!", callback_data='data1')
+    itembtn2 = InlineKeyboardButton(text="Нет, нажми меня!", callback_data='data2')
+    markup.add(itembtn1, itembtn2)
     bot.send_message(message.chat.id, "Привет Нажми на кнопку ниже.", reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def query_callback(call):
-    if call.data == 'data':
+    if call.data == 'data1':
         bot.answer_callback_query(callback_query_id=call.id, text="Вы нажали на кнопку!")
+    else:
+        bot.answer_callback_query(callback_query_id=call.id, text="БЕБЕБЕ")
 
 
 @bot.message_handler(commands=['help'])
