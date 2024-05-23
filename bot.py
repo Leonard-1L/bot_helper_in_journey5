667c54388f6ecc454ff89e766d9657d8471a55eb
ÐLeonard-1L <lprofi52@gmail.com>
2024-05-23 21:19:06 +0300

    видоизменил и продолжил разработку bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import telebot
-from telebot.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from telebot.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, ReplyKeyboardMarkup, \
+    KeyboardButton
 import logging
-from config import LOGS_PATH, BOT_TOKEN
-from database import create_database, add_new_user, update_tokens, change_city, is_user, is_limit_users
+from config import LOGS_PATH, BOT_TOKEN, MAX_USER_TOKENS
+from database import create_database, add_new_user, update_tokens, change_city, is_user, is_limit_users, get_tokens, \
+    get_city
 from gpt import ask_gpt, count_gpt_tokens
 
 logging.basicConfig(
@@ -18,49 +20,84 @@ create_database()
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    if is_limit_users(): #проверка на лимит пользователей
+    if is_limit_users():  # проверка на лимит пользователей
         bot.send_message(message.chat.id, f"К сожалению, у бота слишком много пользователей")
         return
     markup = InlineKeyboardMarkup()
     itembtn1 = InlineKeyboardButton(text="Начинаем!", callback_data='russ')
     markup.add(itembtn1)
-    bot.send_message(message.chat.id, f"Привет, {message.from_user.first_name}! Начинаем?",
+    bot.send_message(message.chat.id,
+                     f"Привет, {message.from_user.first_name}! Я бот такой-то..."  # todo дописать вступление
+                     f"\nНачинаем?",
                      reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: True)
-def query_callback(call):
+def query_callback1(call):
     if call.data:
         msg = bot.send_message(call.message.chat.id,
                                text="Отлично! Я бот, который помогает в путешествиях. Смогу подобрать куда сходить, рассказать про"
                                     "выбранную тобой достопримечательность. \n"
                                     "А теперь напиши город в котором ты сейчас находишься:")
         bot.register_next_step_handler(msg, user_add)
+    else:
+        logging.error("Callback не сработал в начале.")
 
 
 def user_add(message: Message):
-    if is_user(message.from_user.id) == 0: #бот каждый раз добавлял пользователя заново
-        add_new_user(message.from_user.id, message.from_user.first_name) #так что вот проверка
+    if is_user(message.from_user.id) == 0:
+        add_new_user(message.from_user.id, message.from_user.first_name)
+        change_city(user_id=message.from_user.id, new_city=message.text)
+        logging.info(
+            f"{message.from_user.username} c id {message.from_user.id} из города {message.text} теперь с нами!")
+    send_about_city(message)
+
+
+def send_about_city(message: Message):
     change_city(message.from_user.id, message.text)
+    if get_tokens(message.from_user.id) > MAX_USER_TOKENS:
+        bot.send_message(message.from_user.id, "Увы, но у тебя закончились токены на ответ.")
+        return
+
     gpt_bool, gpt_text, gpt_tokens = ask_gpt(f"Расскажи про город {message.text}. Его историю, экономику и население.")
     if gpt_bool:
         update_tokens(user_id=message.from_user.id, add_tokens=gpt_tokens)
         logging.info(
-            f"{message.from_user.username} c id {message.from_user.id} получил ответ {gpt_text}. Токены: {gpt_tokens}")
+            f"{message.from_user.username} c id {message.from_user.id} получил ответ. Затраченные токены: {gpt_tokens}")
+
+    else:
+        logging.error(f'{message.from_user.username} c id {message.from_user.id} не смог получить ответ от нейросети')
+
+    bot.send_message(message.from_user.id, gpt_text)
+    send_categories(message)
+
+
+def send_categories(message: Message):
+    keyboard = ReplyKeyboardMarkup(row_width=2)
+    button1 = KeyboardButton('Повеселиться')
+    button2 = KeyboardButton('Посмотреть достопримечательности')
+    button3 = KeyboardButton('Перекусить')
+    button4 = KeyboardButton('Поспать')
+    keyboard.add(button1, button2, button3, button4)
+    bot.send_message(message.from_user.id, text=f"Выбери, что хочешь сделать в городе {message.text}",
+                     reply_markup=keyboard)
+    bot.register_next_step_handler(message=message, callback=categories_answer)
+
+
+def categories_answer(message: Message):
+    if get_tokens(message.from_user.id > MAX_USER_TOKENS):
+        bot.send_message(message.from_user.id, "Увы, но у тебя закончились токены на ответ.")
+        return
+    gpt_bool, gpt_text, gpt_tokens = ask_gpt(
+        f'Расскажи, где можно погулять в городе {get_city(message.from_user.id)} чтобы {message.text}')
+    if gpt_bool:
+        update_tokens(user_id=message.from_user.id, add_tokens=gpt_tokens)
+        logging.info(
+            f"{message.from_user.username} c id {message.from_user.id} получил ответ {gpt_text}. Затраченные токены: {gpt_tokens}")
+
     else:
         logging.error(f'{message.from_user.username} c id {message.from_user.id} не смог получить ответ от нейросети')
     bot.send_message(message.from_user.id, gpt_text)
-
-    # TODO: на этих кнопках падает бот!
-    markup2 = InlineKeyboardMarkup()
-    itembtn1 = InlineKeyboardButton(text="Поесть", callback_data='пользователь хочет поесть')
-    itembtn2 = InlineKeyboardButton(text="Переночевать", callback_data='пользователь хочет переночевать')
-    itembtn3 = InlineKeyboardButton(text="Посмотреть достопримечательности",
-                                    callback_data='пользователь хочет посмотреть достопримечательности')
-    itembtn4 = InlineKeyboardButton(text="Повеселиться", callback_data='пользователь хочет повеселиться')
-    markup2.add(itembtn1, itembtn4, itembtn3, itembtn2)
-    bot.send_message(message.chat.id, f"Выбери, что хочешь сделать в городе {message.text}",
-                     reply_markup=markup2)
 
 
 @bot.message_handler(commands=['help'])
--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import configparser
 
 config = configparser.ConfigParser()
 
-BOT_TOKEN = '6214808173:AAHmqWl1LL-9ytJ7T5MY9mTnfkvRADIOpv0'#"7032292704:AAFChMlXgB41IlKs8CSOJxYBcB2C2buacpM"
+BOT_TOKEN = "7032292704:AAFChMlXgB41IlKs8CSOJxYBcB2C2buacpM"  # "6214808173:AAHmqWl1LL-9ytJ7T5MY9mTnfkvRADIOpv0"
 
 LOGS_PATH = f'logs.txt'
 IAM_TOKEN_PATH = f'creds/iam_token.json'  # файл для хранения iam_token
@@ -13,13 +13,13 @@ DB_FILE = 'database.db'
 DB_TABLE_NAME = 'users'
 
 MAX_USERS = 10  # лимит на общее количество пользователей
-MAX_ANSWER_GPT_TOKENS = 200
+MAX_USER_TOKENS = 500
 
 config['GPT'] = {
     'URL': 'https://llm.api.cloud.yandex.net/foundationModels/v1/completion',
     'TEMPERATURE': '0.6',
     'TOKENIZE_URL': 'https://llm.api.cloud.yandex.net/foundationModels/v1/tokenizeCompletion',
-    'MAX_ANSWER_GPT_TOKENS': '200'
+    'MAX_ANSWER_GPT_TOKENS': '64'
 }
 
 config['LIMITS'] = {
@@ -28,5 +28,5 @@ config['LIMITS'] = {
 }
 
 SYSTEM_PROMPT = {'role': 'system', 'text': 'Ты веселый помощник по путешествиям.\n'
-                                            'Отвечай интересно.\n'
-                                            'Поддерживай диалог.'}
+                                           'Отвечай интересно.\n'
+                                           'Поддерживай диалог.'}
